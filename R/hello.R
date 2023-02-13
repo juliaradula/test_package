a54e66685e053a8fad1aee342b8816eb795560bf
juliaradula <juliaraspante@gmail.com>
2023-02-13 14:40:23 -0300

    Implemt roxygen2 to the code

--- R/hello.R
+++ R/hello.R
@@ -13,12 +13,22 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-library(usethis)
-library(roxygen2)
-library(devtools)
-library(goodpractice)
-library(BiocCheck)
+# library("roxygen2")
+# Put the cursor inside the function that you
+# want to document and then navigate to ‘Code’
+# and click ‘Insert Roxygen Skeleton’
 
-hello <- function() {
-    print("Hello, world!")
+#' Hello
+#'
+#' \code{hello} returns a greeting sentence incorporating a user-specified string.
+#'
+#' @param your_name A string specifying the name of the person.
+#'
+#' @return A string with the name.
+#' @export
+#'
+#' @examples
+#' hello("Julia")
+hello <- function(your_name) {
+    paste0("Hello, ", your_name)
 }
